a1fb0a7051b0606fbe6be1a3a4aae3694fa10e07
yanto <mrthx.89@gmail.com>
2025-06-20 00:39:30 +0700

    Kategori Add Modul

--- src/Siaga.App/Repository/KategoriAsset.vb
+++ src/Siaga.App/Repository/KategoriAsset.vb
@@ -20,17 +20,48 @@ Namespace Repository
                 Catch ex As Exception
                     Log.Error(ex, "Error : " & ex.Message)
                     XtraMessageBox.Show("Info Kesalahan : " & ex.Message, Application.ProductName)
+                    hasil = New Tuple(Of Boolean, String, Entity.KategoriAsset)(False, "Info Kesalahan : " & ex.Message, Nothing)
                 End Try
             End Using
             Return hasil
         End Function
 
         Public Function [Get](Id As Guid) As Tuple(Of Boolean, String, Entity.KategoriAsset) Implements IKategoriAsset.Get
-            Throw New NotImplementedException()
+            Dim hasil As New Tuple(Of Boolean, String, Entity.KategoriAsset)(False, String.Empty, Nothing)
+            Using dlg As New WaitDialogForm("Sedang merefresh data ...", "Mohon tunggu sebentar")
+                Try
+                    Dim result = DbContext.KategoriAssets.FirstOrDefault(Function(m) m.id = Id)
+                    If (result IsNot Nothing) Then
+                        hasil = New Tuple(Of Boolean, String, Entity.KategoriAsset)(True, "Data ditemukan", result)
+                    Else
+                        hasil = New Tuple(Of Boolean, String, Entity.KategoriAsset)(False, "Data tidak ditemukan", Nothing)
+                    End If
+                Catch ex As Exception
+                    Log.Error(ex, "Error : " & ex.Message)
+                    XtraMessageBox.Show("Info Kesalahan : " & ex.Message, Application.ProductName)
+                    hasil = New Tuple(Of Boolean, String, Entity.KategoriAsset)(False, "Info Kesalahan : " & ex.Message, Nothing)
+                End Try
+            End Using
+            Return hasil
         End Function
 
         Public Function FindByKode(Kode As String) As Tuple(Of Boolean, String, List(Of Entity.KategoriAsset)) Implements IKategoriAsset.FindByKode
-            Throw New NotImplementedException()
+            Dim hasil As New Tuple(Of Boolean, String, Entity.KategoriAsset)(False, String.Empty, Nothing)
+            Using dlg As New WaitDialogForm("Sedang merefresh data ...", "Mohon tunggu sebentar")
+                Try
+                    Dim result = DbContext.KategoriAssets.FirstOrDefault(Function(m) m.kd_kategori = Kode)
+                    If (result IsNot Nothing) Then
+                        hasil = New Tuple(Of Boolean, String, Entity.KategoriAsset)(True, "Data ditemukan", result)
+                    Else
+                        hasil = New Tuple(Of Boolean, String, Entity.KategoriAsset)(False, "Data tidak ditemukan", Nothing)
+                    End If
+                Catch ex As Exception
+                    Log.Error(ex, "Error : " & ex.Message)
+                    XtraMessageBox.Show("Info Kesalahan : " & ex.Message, Application.ProductName)
+                    hasil = New Tuple(Of Boolean, String, Entity.KategoriAsset)(False, "Info Kesalahan : " & ex.Message, Nothing)
+                End Try
+            End Using
+            Return hasil
         End Function
 
         Public Function Create(Data As Entity.KategoriAsset) As Tuple(Of Boolean, String, Entity.KategoriAsset) Implements IKategoriAsset.Create
